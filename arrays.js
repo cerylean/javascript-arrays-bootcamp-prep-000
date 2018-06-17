f0c294788299436b6789344897a9637661811fd9
Lyssa Treibitz <cerylean@gmail.com>
2018-06-17 16:26:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,4 +12,4 @@ function accessElementInArray(array, index) {return array[index]}
 
 function destructivelyRemoveElementFromBeginningOfArray(array) { array.shift(); return array}
 
-function removeElementFromBeginningOfArray(array) { array.slice(1); return array}
+function removeElementFromBeginningOfArray(array) {return array.slice(1)}
