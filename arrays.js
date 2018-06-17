c3e07971dac8dd029cb928c7be43422bad261fd8
Lyssa Treibitz <cerylean@gmail.com>
2018-06-17 16:15:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ function addElementToBeginningOfArray(array, element) {array=[element,...array];
 
 function destructivelyAddElementToBeginningOfArray(array, element) { array.unshift(element); return array}
 
-function addElementToBeginningOfArray(array, element) {array=[...array,element]; return array}
+function addElementToEndOfArray(array, element) {array=[...array,element]; return array}
 
-function destructivelyAddElementToBeginningOfArray(array, element) { array.push(element); return array}
+function destructivelyAddElementToEndOfArray(array, element) { array.push(element); return array}
